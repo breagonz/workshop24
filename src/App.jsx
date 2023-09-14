1199ab48499a65b5de953e1b5f019f3977b70777
breanna gonzales <115334210+breagonz@users.noreply.github.com>
2023-09-13 20:08:33 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
   return (
     <>
-      <div>
-
-      
+      <div>{
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })
+        }      
       </div>
       
     </>
